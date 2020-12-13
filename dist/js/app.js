1688db2c861f02c1b9ed6e649c7b3bbc0ac14031
David Guzman <davidguzmaninc@gmail.com>
2020-12-13 00:23:06 -0500

    Added a theme switcher

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,7 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-item');
-    const body = document.querySelector('body')
+    const body = document.querySelector('body');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
@@ -16,18 +16,55 @@ const navSlide = () => {
                 }s`;
             }
 
-            if(nav.classList.contains('nav-active')) {
-                body.style.overflow = 'hidden'
-            }
-            else {
-                body.style.overflow = 'auto'
+            if (nav.classList.contains('nav-active')) {
+                body.style.overflow = 'hidden';
+            } else {
+                body.style.overflow = 'auto';
             }
         });
     });
 };
 
+const switchTheme = () => {
+    // Elements
+    const theme = document.querySelector('.theme');
+    const body = document.querySelector('body');
+    const links = document.querySelectorAll('a');
+    const logo = document.querySelector('.logo');
+    const burgerDiv = document.querySelector('.burger div');
+    const textBlock = document.querySelector('.text-block');
+
+    // Colors
+    const darkColor = '#181719';
+    const lightColor = '#eee';
+
+
+    theme.addEventListener('click', () => {
+        if (theme.classList.contains('fa-moon')) {
+            theme.classList.remove('fa-moon');
+            theme.classList.add('fa-sun');
+            body.style.backgroundColor = lightColor;
+            body.style.color = darkColor;
+            logo.style.borderColor = darkColor;
+            burgerDiv.style.backgroundColor = darkColor;
+            textBlock.style.backgroundColor = lightColor;
+            links.forEach((link) => (link.style.color = darkColor));
+        } else {
+            theme.classList.remove('fa-sun');
+            theme.classList.add('fa-moon');
+            body.style.backgroundColor = darkColor;
+            body.style.color = lightColor;
+            logo.style.border = `1px solid ${lightColor}`;
+            burgerDiv.style.backgroundColor = lightColor;
+            textBlock.style.backgroundColor = darkColor;
+            links.forEach((link) => (link.style.color = lightColor));
+        }
+    });
+};
+
 const app = () => {
     navSlide();
+    switchTheme();
 };
 
 app();
