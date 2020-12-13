fadb9885003dac58fdb8b0386e7aad72f4fec3cc
David Guzman <davidguzmaninc@gmail.com>
2020-12-12 21:38:53 -0500

    got everything mobile responsive

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,6 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-item');
+    const body = document.querySelector('body')
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
@@ -13,6 +14,13 @@ const navSlide = () => {
                 link.style.animation = `NavLinkFade 0.5s ease forwards ${
                     idx / 5 + 0.3
                 }s`;
+            }
+
+            if(nav.classList.contains('nav-active')) {
+                body.style.overflow = 'hidden'
+            }
+            else {
+                body.style.overflow = 'auto'
             }
         });
     });
